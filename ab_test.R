cb6f99535a27ac9e29cfb6bbf8ac92c9075b6ecc
Lachlan Deer <lachlan.deer@gmail.com>
2023-04-23 20:32:51 +0200

    student starter code

--- ab_test.R
+++ ab_test.R
@@ -32,10 +32,7 @@ df <-
     mutate(sale_indicator = if_else(sales > 0, 1, 0))
 
 #--- Descriptive Statistics --- #
-skim(df)
-
-# summarize data by version
-datasummary_balance(~version, data = df)
+YOUR CODE
 
 # --- Plot Distribution of Sales ---# 
 df %>%
@@ -46,104 +43,22 @@ df %>%
     theme_bw()
 
 # --- T-test, difference in means --- #
-df %>% 
-    group_by(version) %>%
-    summarize(mean_sales = mean(sales, na.rm = TRUE))
-
-t_test(df, 
-       sales ~ version, 
-       order = c("A", "B"), 
-       var.equal = FALSE
-       )
+YOUR CODE
 
 # --- Simple Linear Regression ---# 
-model_simple <- lm(sales ~ version, data = df)
-summary(model_simple)
-tidy(model_simple)
-glance(model_simple)
-
-# With a binary y?
-model_simple_binary <- lm(sale_indicator ~ version, data = df)
-tidy(model_simple_binary)
-glance(model_simple_binary)
-
-# What about conditional on any sales being +ve?
-model_conditional <-
-    df %>%
-    filter(sales > 0) %>%
-    lm(sales ~ version, data = .)
-    
-tidy(model_conditional)
-
-# log y, with conditional sales
-model_log_conditional <-
-    df %>%
-    filter(sales > 0) %>%
-    lm(log(sales) ~ version, data = .)
-
-tidy(model_log_conditional)
+YOUR CODE
 
 # --- Regression by Group --- #
 # Are there differences between email and internet
-model_by_source <- 
-    df %>%
-    group_by(source) %>%
-    nest() %>%
-    mutate(
-        model = map(data, ~ lm(sales  ~ version, data = .))
-    )
-
-model_by_source %>%
-    mutate(tidy_mod = map(model, tidy)) %>%
-    select(tidy_mod) %>%
-    unnest(cols = c(tidy_mod))
-
-# conditional on +ve sale?
-model_by_source <- 
-    df %>%
-    filter(sales > 0) %>%
-    group_by(source) %>%
-    nest() %>%
-    mutate(
-        model = map(data, ~ lm(log(sales)  ~ version, data = .))
-    )
-
-model_by_source %>%
-    mutate(tidy_mod = map(model, tidy)) %>%
-    select(tidy_mod) %>%
-    unnest(cols = c(tidy_mod))
-
-# probability of a sale?
-model_by_source <- 
-    df %>%
-    group_by(source) %>%
-    nest() %>%
-    mutate(
-        model = map(data, ~ lm(sale_indicator  ~ version, data = .))
-    )
-
-model_by_source %>%
-    mutate(tidy_mod = map(model, tidy)) %>%
-    select(tidy_mod) %>%
-    unnest(cols = c(tidy_mod))
+YOUR CODE
 
 # --- Displaying Regression Output ---# 
 # Regression Table
-models <- list(model_simple, 
-               model_conditional,
-               model_log_conditional)
-
-modelsummary(models = dvnames(models),
-             coef_rename  = c("versionB" = "Version B"),
-             fmt = 2,
-             coef_omit = "Intercept",
-             gof_omit = 'AIC|BIC|F|RMSE|Log')
+YOUR CODE
 
 # Coefficient Plot
-modelplot(model_by_source$model, 
-          coef_omit = 'Interc'
-            )
-    
+YOUR CODE
+
 # --- Heteroskedasticity and Robust Standard Errors --- #
 # Evidence of heteroskdasticty?
 df <- 
@@ -156,9 +71,9 @@ df %>%
   geom_point(aes(y = resid, x = predict))
 
 # A "test" for heteroskedasticity
-lmtest::bptest(model_simple)
+YOUR CODE
 
 # Heteroskedasticity Robust Standard Errors
-lm_robust(sales~version, data = df)
-lm_robust(sales~version, data = df, se_type = "HC3")
+YOUR CODE
+
 
--- did.R
+++ did.R
@@ -24,11 +24,7 @@ df <-
   mutate(post = if_else(month >= 19, 1, 0))  
 
 # --- The Diff-in-Diff Table --- #
-treat_table <-
-  df %>%
-  group_by(post, treat) %>%
-  summarise(qty = mean(qty_purchases)) %>%
-  ungroup()
+YOUR CODE
 
 # --- "Eyeconometrics" of Diff in Diff --- #
 treat_table %>%
@@ -46,21 +42,10 @@ treat_table %>%
     )
 
 # --- Diff in Diff via the Treatment Table --- #
-treat_table %>%
-  # makes treat and control group each on column
-  tidyr::pivot_wider(names_from = treat,
-              values_from = qty
-  ) %>%
-  mutate_all(list(~ . - lag(.))) %>%
-  mutate(did_simple = `1` - `0`) %>%
-  na.omit() %>%
-  select(did_simple)
+YOUR CODE
 
 # --- Diff in Diff as a Regression --- #
-did_simple <-
-    lm(qty_purchases ~  treat + post + treat:post, data = df)
-
-tidy(did_simple)    
+YOUR CODE
 
 # --- Econometrics Redux --- #
 df %>%
@@ -74,71 +59,21 @@ df %>%
               ) + 
     geom_vline(xintercept = 19, color = "red", linestyle = '--')
 
-# --- Add a Common monthly 'trend' to the data --- #
-model_monthly <- 
-    lm(qty_purchases ~  treat + post + treat:post + 
-           factor(month), 
-            data = df
-       )
+# --- Add a common monthly fixed effects to the data --- #
+YOUR CODE
 
-tidy(model_monthly)
-
-# Equivalent to
-model_month_fe <- 
-    feols(qty_purchases ~  treat + post + treat:post | month, 
-            vcov = "iid",
-            data = df
-          )
-
-tidy(model_month_fe)
-
-# is this a large effect?
-df %>%
-    filter(post == 0) %>%
-    summarise(sd_qty = sd(qty_purchases))
 
 # --- Adding User Fixed effects --- #
-model_user_fe <- 
-    feols(qty_purchases ~  treat + post + treat:post | month + id, 
-          vcov = "iid",
-          data = df
-    )
+YOUR CODE
 
-tidy(model_user_fe)
 
 # --- Heteroskedasticity Redux --- #
-
-model_hetero <-
-    feols(qty_purchases ~  treat + post + treat:post | month + id, 
-          vcov = "hetero",
-          data = df
-          )
-
-tidy(model_hetero)
+YOUR CODE
 
 # --- Clustered Standard Errors --- #
-model_cluster_id  <- 
-    feols(qty_purchases ~  treat + post + treat:post | month + id, 
-          cluster = ~id,
-          data = df
-          )
+YOUR CODE
 
-tidy(model_cluster_id)
-
-model_cluster_zip  <- 
-    feols(qty_purchases ~  treat + post + treat:post | month + id, 
-          cluster = ~zip,
-          data = df
-    )
-
-tidy(model_cluster_id)
 
 # --- Is the Treatment Effect Constant Over Time --- #
-model_event <- 
-    feols(qty_purchases ~  i(month, treat, 19)  | month + id, 
-            cluster = ~zip,
-            data = df)
+YOUR CODE
 
-tidy(model_event)
-
-iplot(model_event)
